554dd400c087aae028cb6a2f78552fb40f21bea7
zhenglli <123@qq.com>
2020-01-03 09:55:51 +0800

    修改端口号-更新package.json文件

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ var logisticsRouter = require('./routes/logistics')
 
 var app = express();
 app.use(cors({
-  origin:["http://127.0.0.1:8080","http://localhost:8080","http://49.235.127.99:3389"],
+  origin:["http://127.0.0.1:8081","http://localhost:8081","http://49.235.127.99:3389"],
   credentials:true
 }));
 app.use(logger('dev'));
